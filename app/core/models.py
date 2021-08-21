90f88da4be4c62a59d08777c453d29689c5eac61
Archana Sharma <88829538+Archana-Airbase@users.noreply.github.com>
2021-08-22 00:50:03 +0530

    Added custom user model

---
+++ app/core/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.forms.fields import CharField
+
+# Create your models here.
+
+class UserManager(BaseUserManager):
+    """User Manager Class
+    """
+
+    def create_user(self,email,password,**extra_fields):
+        """Create a new user
+
+        Args:
+            email (string): user email address
+            password (string): user password
+        """
+
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user=self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_super_user(self,email,password):
+
+        """Create new super user"""
+        
+        user=self.create_user(email,password)
+        user.is_staff=True
+        user.is_superuser=True
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """Custom user model that supports email instead of username
+    """
+    email=models.EmailField(max_length=255,unique=True)
+    name=models.CharField(max_length=255)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=True)
+
+    objects=UserManager()
+    
+    USERNAME_FIELD='email'
